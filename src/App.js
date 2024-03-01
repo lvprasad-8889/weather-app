19530265f405e44435a20630e9a4980f724e2260
lvprasad-8889 <lvprasad8889@gmail.com>
2024-03-01 10:41:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import Search from'./components/search/search';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/reducer.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: false,
+  cityName: "",
+  cityDetails: [],
+  currentItem: {},
+  showTopFive: true,
+  topFive: [],
+};
+
+const weatherSlice = createSlice({
+  name: "weather",
+  initialState,
+  reducers: {
+    setIsLoading(state, action) {
+      state.isLoading = action.payload.data;
+    },
+    setCityDetails(state, action) {
+      state.cityDetails = action.payload.data;
+      state.currentItem = state.cityDetails[0];
+      state.cityDetails = state.cityDetails.slice(0, 5);
+    },
+    setCityName(state, action) {
+      state.cityName = action.payload.data;
+    },
+    setCurrentItem(state, action) {
+      state.currentItem = state.cityDetails[action.payload.data];
+    },
+    setShowTopFive(state, action) {
+      state.showTopFive = action.payload.data;
+    },
+    toggleTopFive(state, action) {
+      state.showTopFive = !state.showTopFive;
+    },
+  },
+});
+
+export const weatherActions = weatherSlice.actions;
+
+const weatherReducer = weatherSlice.reducer;
+
+export default weatherReducer;
