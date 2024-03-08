fa9b8a50fb6b5ad03f206fcdd0f48b2776c27062
lvprasad-8889 <lvprasad8889@gmail.com>
2024-03-08 23:39:48 +0530

    Changed weather logo

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,8 +8,8 @@ const Navbar = () => {
   let currentCity = useSelector((state) => state.weather.cityName);
   return (
     <nav className="navbar bg-white navbar-expand mb-3">
-      <div className="container d-flex justify-content-between">
-        <div className="navbar-brand fw-bold d-flex justify-content-between align-items-center">
+      <div className="container d-flex justify-content-around">
+        <div className="navbar-brand fw-bold d-flex justify-content-between align-items-center gap-sm-2">
           <img src={Logo} alt="" width="50px" height="40px" />
           WeatherApp
         </div>
